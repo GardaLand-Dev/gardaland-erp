995e59dff01532b8b32f1d26dbbc673549f06eba
yasser hamida <yasser.hamida08@gmail.com>
2020-08-12 17:58:58 +0100

    disabled sequilize loggin, db init is now a function

--- app/backend/db/models/index.ts
+++ app/backend/db/models/index.ts
@@ -20,6 +20,7 @@ const dbConfig = (() => {
       dialect: 'sqlite',
       dialectModulePath: '@journeyapps/sqlcipher',
       storage: './db.sqlite',
+      logging: () => {},
     }
   );
   sequilze.query(`PRAGMA key="${process.env.DB_PASSWORD}"`);
@@ -43,37 +44,40 @@ export const Resource = ResourceFactory(dbConfig);
 export const Privilege = PrivilegeFactory(dbConfig);
 export const Role = RoleFactory(dbConfig);
 export const User = UserFactory(dbConfig);
+export const dbInit = () => {
+  /* RBAC - RELATIONS */
+  /** role-user */
+  User.belongsToMany(Role, { through: 'User_Role' });
+  Role.belongsToMany(User, { through: 'User_Role', onDelete: 'RESTRICT' });
+  /** role-privilege */
+  Role.belongsToMany(Privilege, {
+    through: 'Role_Privilege',
+    onDelete: 'CASCADE',
+  });
+  Privilege.belongsToMany(Role, {
+    through: 'Role_Privilege',
+    onDelete: 'RESTRICT',
+  });
+  /** privilege-resource */
+  Privilege.belongsTo(Resource);
+  Resource.hasMany(Privilege, {
+    foreignKey: {
+      allowNull: false,
+    },
+    onDelete: 'RESTRICT',
+  });
+  /** privilege-Operation */
+  Privilege.belongsTo(Operation);
+  Operation.hasMany(Privilege, {
+    foreignKey: {
+      allowNull: false,
+    },
+    onDelete: 'RESTRICT',
+  });
 
-/* RBAC - RELATIONS */
-/** role-user */
-User.belongsToMany(Role, { through: 'User_Role' });
-Role.belongsToMany(User, { through: 'User_Role', onDelete: 'RESTRICT' });
-/** role-privilege */
-Role.belongsToMany(Privilege, {
-  through: 'Role_Privilege',
-  onDelete: 'CASCADE',
-});
-Privilege.belongsToMany(Role, {
-  through: 'Role_Privilege',
-  onDelete: 'RESTRICT',
-});
-/** privilege-resource */
-Privilege.belongsTo(Resource);
-Resource.hasMany(Privilege, {
-  foreignKey: {
-    allowNull: false,
-  },
-  onDelete: 'RESTRICT',
-});
-/** privilege-Operation */
-Privilege.belongsTo(Operation);
-Operation.hasMany(Privilege, {
-  foreignKey: {
-    allowNull: false,
-  },
-  onDelete: 'RESTRICT',
-});
-
-/* SYNCING */
-
-dbConfig.sync();
+  /* SYNCING */
+  dbConfig
+    .sync()
+    .then(() => console.log('database created and syncronized'))
+    .catch((err) => console.log('couldnt synchronize db', err));
+};
