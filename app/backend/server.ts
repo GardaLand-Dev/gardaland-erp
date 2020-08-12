ffb5954a0f8ae620b0dab1e56e71df08dc72794f
yasser hamida <yasser.hamida08@gmail.com>
2020-08-12 18:04:49 +0100

    added auth and rolecheck middlewares

--- app/backend/server.ts
+++ app/backend/server.ts
@@ -1,49 +1,41 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import { dbInit } from './db/models';
+import authCheck, { notAuthHandler } from './middlewares/authCheck';
+import rbacInit from './middlewares/rbac';
+
 import TestRoutes from './routes/test_route';
 import CommonRoutes from './routes/common_routes';
-import UserRoute from './routes/user_route';
-// import sqlite3 from '@journeyapps/sqlcipher';
+import RBACRoutes from './routes/rbac_routes';
+import LoginRoute from './routes/login_route';
 
-export class Server {
+class Server {
   public app: express.Application;
 
   private test_routes: TestRoutes;
 
   private common_routes: CommonRoutes;
 
-  // private user_route: UserRoute;
-
-  // private db: sqlite3.default;
-
   constructor() {
+    // init
+    rbacInit(dbInit);
+    process.env.PRIVATE_KEY = 'yasser9999';
     this.test_routes = new TestRoutes();
     this.common_routes = new CommonRoutes();
-    // this.user_route = new UserRoute();
     this.app = express();
     this.config();
-    this.test_routes.route(this.app);
-    UserRoute.route(this.app);
-    this.common_routes.route(this.app);
   }
 
   private config(): void {
     this.app.use(bodyParser.json());
     this.app.use(bodyParser.urlencoded({ extended: true }));
+    this.app.use(authCheck);
+    this.app.use(notAuthHandler);
+    this.test_routes.route(this.app);
+    LoginRoute.route(this.app);
+    RBACRoutes.route(this.app);
+    // this.common_routes.route(this.app);
   }
 }
-
 export default new Server().app;
-// const app: express.Application = express.default();
-// const port = 3333;
-
-// export default function startServer() {
-//   app.get('/', (_req: unknown, res: { send: (arg0: string) => void }) => {
-//     res.send('Hello World!');
-//   });
-
-//   app.listen(port, () => {
-//     // eslint-disable-next-line no-console
-//     console.log(`Example app listening at http://localhost:${port}`);
-//   });
-// }
+// export default new Server().app;
