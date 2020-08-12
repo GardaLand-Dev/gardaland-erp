73fce7ca38a5f922dc8102e9b5d7a3b6b247ea13
yasser hamida <yasser.hamida08@gmail.com>
2020-08-12 18:03:07 +0100

    add jwt auth checking middleware

---
+++ app/backend/middlewares/authCheck.ts
@@ -0,0 +1,37 @@
+import ejwt from 'express-jwt';
+import { Request, Response, NextFunction } from 'express';
+import { unauthorizedRequest } from '../controllers/common/service';
+
+const reqProp = 'auth';
+
+const config = {
+  secret: 'yasser9999',
+  algorithms: ['HS256'], // use RS256 for asymmetric algorithm
+  requestProperty: reqProp,
+};
+
+export interface JwtRequest extends Request {
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  [reqProp]?: string | object;
+}
+
+export default ejwt(config).unless({
+  path: ['/token', '/api/login'],
+});
+
+export const notAuthHandler = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (
+    err.name === 'UnauthorizedError' &&
+    !(req.path.endsWith('/token') || req.path.endsWith('/api/login'))
+  ) {
+    unauthorizedRequest(res);
+  } else next();
+};
+
+// TODO: see this https://www.digitalocean.com/community/tutorials/nodejs-jwt-expressjs
+// TODO: https://jasonwatmore.com/post/2017/12/07/react-redux-jwt-authentication-tutorial-example
