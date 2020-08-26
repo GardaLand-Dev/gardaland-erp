45654605e9fe82f5b0105db2b7349fd48c4b0f9a
yasser hamida <yasser.hamida08@gmail.com>
2020-08-27 00:42:18 +0100

    added statics.service

--- app/services/statics.service.ts
+++ app/services/statics.service.ts
@@ -18,6 +18,23 @@ function createStation(name: string, printerName = null) {
     })
     .catch((err) => err);
 }
+function getStations(all = false, limit = null, page = null) {
+  const requestOptions = {
+    method: 'GET',
+    headers: {
+      'Content-type': 'application/json',
+      Authorization: authHeader().Authorization,
+    },
+    body: JSON.stringify({
+      all,
+      limit,
+      page,
+    }),
+  };
+  return fetch(`${config.apiUrl}/stations`, requestOptions)
+    .then((res: Response) => res.json())
+    .catch((err) => err);
+}
 
 /** family */
 function createFamily(name: string, stationId = null) {
@@ -30,13 +47,128 @@ function createFamily(name: string, stationId = null) {
     body: JSON.stringify({ name, stationId }),
   };
   return fetch(`${config.apiUrl}/family`, requestOptions)
-    .then((res: Response) => {
-      return res.ok;
-    })
+    .then((res: Response) => res.ok)
+    .catch((err) => err);
+}
+function getFamilies(all = false, limit = null, page = null) {
+  const requestOptions = {
+    method: 'GET',
+    headers: {
+      'Content-type': 'application/json',
+      Authorization: authHeader().Authorization,
+    },
+    body: JSON.stringify({
+      all,
+      limit,
+      page,
+    }),
+  };
+  return fetch(`${config.apiUrl}/familiess`, requestOptions)
+    .then((res: Response) => res.json())
+    .catch((err) => err);
+}
+
+/** product */
+function createProduct(
+  name: string,
+  priceTtc: number,
+  tva: number = null,
+  ingredients: Array<{
+    stockableId: string;
+    quantity: number;
+  }>,
+  isComposed: boolean,
+  stockableId: string = null,
+  familyId: string
+) {
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json',
+      Authorization: authHeader().Authorization,
+    },
+    body: JSON.stringify({
+      name,
+      priceTtc,
+      tva,
+      isComposed,
+      ingredients,
+      stockableId,
+      familyId,
+    }),
+  };
+  return fetch(`${config.apiUrl}/product`, requestOptions)
+    .then((res: Response) => res.ok)
+    .catch((err) => err);
+}
+function getProducts(all = false, limit = null, page = null) {
+  const requestOptions = {
+    method: 'GET',
+    headers: {
+      'Content-type': 'application/json',
+      Authorization: authHeader().Authorization,
+    },
+    body: JSON.stringify({
+      all,
+      limit,
+      page,
+    }),
+  };
+  return fetch(`${config.apiUrl}/products`, requestOptions)
+    .then((res: Response) => res.json())
+    .catch((err) => err);
+}
+
+/** suppliment */
+function createSuppliment(
+  name: string,
+  price: number,
+  quantity: number,
+  stockableId: string
+) {
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json',
+      Authorization: authHeader().Authorization,
+    },
+    body: JSON.stringify({
+      name,
+      quantity,
+      price,
+      stockableId,
+    }),
+  };
+  return fetch(`${config.apiUrl}/suppliment`, requestOptions)
+    .then((res: Response) => res.ok)
+    .catch((err) => err);
+}
+function getSuppliments(all = false, limit = null, page = null) {
+  const requestOptions = {
+    method: 'GET',
+    headers: {
+      'Content-type': 'application/json',
+      Authorization: authHeader().Authorization,
+    },
+    body: JSON.stringify({
+      all,
+      limit,
+      page,
+    }),
+  };
+  return fetch(`${config.apiUrl}/suppliments`, requestOptions)
+    .then((res: Response) => res.json())
     .catch((err) => err);
 }
 
 const staticService = {
+  createStation,
+  getStations,
   createFamily,
+  getFamilies,
+  createProduct,
+  getProducts,
+  createSuppliment,
+  getSuppliments,
 };
 export default staticService;
